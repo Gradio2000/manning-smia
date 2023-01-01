e846f93d0392a48614d0475919d4aec2010a1d89
aleksejlaskin <laskin999@gmail.com>
2023-01-01 10:36:08 +0500

    5 глава

--- chapter5/licensing-service/src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
+++ chapter5/licensing-service/src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
@@ -4,8 +4,10 @@ import java.util.Locale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
